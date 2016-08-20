eb67897f17d3da7edb88818c7e3b22d5b2a8f0ac
lizhaosheng <withfeelings@163.com>
2016-08-20 17:06:30 +0800

    jdbc

--- gateway/src/main/java/com/lzs/puppet/gateway/Application.java
+++ gateway/src/main/java/com/lzs/puppet/gateway/Application.java
@@ -4,6 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 
+/**
+ * @EnableSidecar annotation includes @EnableCircuitBreaker, @EnableDiscoveryClient, and @EnableZuulProxy
+ * ClassName: Application <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON(optional). <br/>
+ * date: 2016年8月20日 下午2:47:49 <br/>
+ *
+ * @author lizhaosheng-黎昭声
+ * @version 
+ * @since JDK 1.6
+ */
 @SpringBootApplication
 @EnableSidecar
 public class Application {
